fbde2e57d05ffea8c88afaaa6c00061f4b9697e7
Nathan Tansey <78175836+nathanjtansey@users.noreply.github.com>
2023-05-01 14:41:20 -0500

    change color palette, reverse legend order

--- Custom_Graphs.py
+++ Custom_Graphs.py
@@ -14,7 +14,7 @@ with header:
     st.write("""Selecting the fields below will filter the data to use for the below visualizations. In addition to the
      dropdown menus, Plotly graphs are customizable or downloadable using the menu on each individual graph.""")
     st.write("""Clicking on a certain subfield in the Plotly menu will remove the subfield from the graph, allowing for an
-    alternative method of tuning visualizations. """) 
+    alternative method of tuning visualizations. Double clicking on a subfield will only plot that specific subfield. """) 
 
 with selection_section:
     
@@ -25,23 +25,22 @@ with selection_section:
     # set up columns for widgets
     col1, col2 = st.columns(2)
     
+
+    with col1: # filters the dataframe before selecting variables to select data based on non-graphing variables
+        df_filtered = filter_dataframe(df)
     # option to select columns to exclude from dataframe
-    with col1:
+    with col2:
         select_cols = st.checkbox("Click Here to Select Variables")
         if select_cols:
             select_cols = st.multiselect("Select Columns:", # Limits selectable variables for graphing
                                      ['docYear', 'origin', 'PrecedentialStatus', 'DispGeneral'])
             df_cols = ['uniqueID'] + select_cols
             # include selected columns
-            df_cols = [col for col in df.columns if col in df_cols]
+            df_cols = [col for col in df_filtered.columns if col in df_cols]
             # return dataframe with selected columns
-            df = df[df_cols]
+            df_filtered = df_filtered[df_cols]
+    #st.dataframe(df_filtered.head(), use_container_width = True)
     
-    # convert data to streamlit DataFrame with filtering options
-    with col2:
-        df_filtered = filter_dataframe(df)
-    st.dataframe(df_filtered.head(), use_container_width = True)
-
     
 with graph_section:
     if select_cols: # checks whether any columns have been selected
@@ -49,24 +48,35 @@ with graph_section:
         # this if elif list can be changed in priority to show certain graphs over others
         # earlier statements have higher priority
         if 'origin' in df_cols and 'DispGeneral' in df_cols: # graphs only if certain columns were selected
-            df8 = df_filtered.groupby(['docYear','DispGeneral']).count().reset_index()
-            df8 = df8.rename(columns = {'uniqueID': 'Count'})
-            figc = px.bar(df8, x = 'docYear', y = 'Count', color = 'DispGeneral', title = 'Dispositions')
-            #st.plotly_chart(fig8)
+            dfoD = df_filtered.groupby(['docYear','DispGeneral']).count().reset_index()
+            dfoD = dfoD.rename(columns = {'uniqueID': 'Count'}) 
+            figc = px.bar(dfoD, x = 'docYear', y = 'Count', color = 'DispGeneral', title = 'Dispositions',
+                          color_discrete_sequence=px.colors.qualitative.Light24[2:])
+            figc.update_layout(legend_traceorder="reversed")  # First plotted is at the bottom of legend      
         elif 'docYear' in df_cols and 'origin' in df_cols:
             yearXorigin_df = df_filtered.groupby(['docYear','origin']).count()
             yearXorigin_df = yearXorigin_df.rename(columns = {'uniqueID': 'Count'})
             figc = px.bar(yearXorigin_df.reset_index(), x = 'docYear', y = 'Count', 
-                               color = 'origin', title = 'Court Origins over Time')        
-        elif 'PrecedentialStatus' in df_cols and 'DispGeneral' in df_cols:
-            # prec = df_filtered.loc[df_filtered['PrecedentialStatus'] == 'Precedential']
+                               color = 'origin', title = 'Court Origins over Time',
+                               color_discrete_sequence=px.colors.qualitative.Light24[0:])
+            figc.update_layout(legend_traceorder="reversed")
+        elif 'DispGeneral' in df_cols:
             prec_gp = df_filtered.groupby(['DispGeneral']).count().reset_index()
             prec_gp = prec_gp.rename(columns = {'uniqueID': 'Count'})
-            figc = px.pie(prec_gp, values = 'Count', names = 'DispGeneral',title = 'Case Results')
+            figc = px.pie(prec_gp, values = 'Count', names = 'DispGeneral',title = 'Case Results',
+                          color_discrete_sequence=px.colors.qualitative.Light24[2:])
         elif 'origin' in df_cols:
             origin_df = df_filtered.groupby('origin').count()
-            figc = px.bar(origin_df.reset_index(), x = 'origin', y = 'uniqueID', title = 'Court Origins')
-            #st.plotly_chart(fig_o)
+            figc = px.bar(origin_df.reset_index(), x = 'origin', y = 'uniqueID', title = 'Court Origins',
+                          color_discrete_sequence=px.colors.qualitative.Light24[2:])
+            figc.update_layout(legend_traceorder="reversed")
+        elif 'docYear' in df_cols:
+            year_df = df_filtered.groupby(['docYear']).count().reset_index()
+            year_df = year_df.rename(columns = {'uniqueID': 'Count'})
+            figc = px.bar(year_df, x = 'docYear', y = 'Count', title = 'Number of Court Cases',
+                          color_discrete_sequence=px.colors.qualitative.Light24[2:])
+            figc.update_layout(legend_traceorder="reversed")
+            
 
         try: # checks if a plot has been generated 
             #figc
